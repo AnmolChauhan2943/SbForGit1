81349c3043457e4fdfbf08e475211762819ab61a
AnmolChauhan2943 <anmol.chauhan2943@gmail.com>
2024-12-09 15:35:08 +0530

    2. Second Commit

--- src/sb/for_git/GitDemo.java
+++ src/sb/for_git/GitDemo.java
@@ -1,10 +1,16 @@
 package sb.for_git;
 
+import sb.for_git.ex_1.Student;
+
 public class GitDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		System.out.println("\n This project is to learn git");
+		
+		Student student = new Student(101, "Aakriti");
+		System.out.println(student);
 	}
 
 }
